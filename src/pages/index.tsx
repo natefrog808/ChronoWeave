ee5382395f10765377e346fff6046a50cc992115
natefrog808 <wetakatini@gmail.com>
2025-02-20 09:20:21 -1000

    Create index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import AdvancedTimeline from '@/components/timeline/AdvancedTimeline';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <AdvancedTimeline 
+        showConnections={true}
+        enableZoom={true}
+        allowFiltering={true}
+        interactive={true}
+      />
+    </main>
+  );
+}
